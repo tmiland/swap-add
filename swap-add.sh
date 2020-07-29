1228e84781292112b357854938d92138d43e2c4f
Tommy Miland <kontakt@tmiland.com>
2020-07-29 12:34:22 +0200

    Update swap-add.sh

--- swap-add.sh
+++ swap-add.sh
@@ -37,69 +37,69 @@ check_root(){
 }
 
 check_memory_and_swap(){
-	mem_count=`free -m | grep Mem | awk '{print $2}'`
-	swap_count=`free -m|grep Swap|awk '{print $2}'`
+	mem_count=$(free -m | grep Mem | awk '{print $2}')
+	swap_count=$(free -m|grep Swap|awk '{print $2}')
 	if [ "$mem_count" -ge 15000 ]  && [ "$mem_count" -le 32768 ]
 	then
 		if [ "$swap_count" -ge 8000 ]
 		then
-			echo -e "\033[1;40;31mYour swap is already enough.Do not need to add swap.Script will exit.\n\033[0m"
+			echo -e "\033[1;40;31mAlready enough swap space, no need to add swap. Script will exit.\n\033[0m"
 			rm -rf $LOCKfile
 			exit 1
 		elif [ "$swap_count" -ne 0 ]
 		then
-			echo -e "\033[40;32mYour swap is not enough,need to add swap.\n\033[40;37m"
+			echo -e "\033[40;32mNot enough swap space, adding swap.\n\033[40;37m"
 			remove_old_swap
 			create_swap 8192
 		else
-			echo -e "\033[40;32mYour swap is not enough,need to add swap.\n\033[40;37m"
+			echo -e "\033[40;32mNot enough swap space, adding swap.\n\033[40;37m"
 			create_swap 8192
 		fi
 	elif [ "$mem_count" -ge 3900 ] && [ "$mem_count" -lt 15000 ]
 	then
 		if [ "$swap_count" -ge 3900 ]
 		then
-			echo -e "\033[1;40;31mYour swap is already enough.Do not need to add swap.Script will exit.\n\033[0m"
+			echo -e "\033[1;40;31mAlready enough swap space, no need to add swap. Script will exit.\n\033[0m"
 			rm -rf $LOCKfile
 			exit 1
 		elif [ "$swap_count" -ne 0 ]
 		then
-			echo -e "\033[40;32mYour swap is not enough,need to add swap.\n\033[40;37m"
+			echo -e "\033[40;32mNot enough swap space, adding swap.\n\033[40;37m"
 			remove_old_swap
 			create_swap 4096
 		else
-			echo -e "\033[40;32mYour swap is not enough,need to add swap.\n\033[40;37m"
+			echo -e "\033[40;32mNot enough swap space, adding swap.\n\033[40;37m"
 			create_swap 4096
 		fi
 	else
 		if [ "$swap_count" -ge 2000 ]
 		then
-			echo -e "\033[1;40;31mYour swap is already enough.Do not need to add swap.Script will exit.\n\033[0m"
+			echo -e "\033[1;40;31mAlready enough swap space, no need to add swap. Script will exit.\n\033[0m"
 			rm -rf $LOCKfile
 			exit 1
 		elif [ "$swap_count" -ne 0 ]
 		then
-			echo -e "\033[40;32mYour swap is not enough,need to add swap.\n\033[40;37m"
+			echo -e "\033[40;32mNot enough swap space, adding swap.\n\033[40;37m"
 			remove_old_swap
 			create_swap 2048
 		else
-			echo -e "\033[40;32mYour swap is not enough,need to add swap.\n\033[40;37m"
+			echo -e "\033[40;32mNot enough swap space, adding swap.\n\033[40;37m"
 			create_swap 2048
 		fi
 	fi
 }
 
 create_swap(){
-	root_disk_size=`df -m|grep -w "/"|awk '{print $4}'`
+	root_disk_size=$(df -m|grep -w "/"|awk '{print $4}')
 	if [ "$1" -gt "${root_disk_size-1024}" ]
 	then
-		echo -e "\033[1;40;31mThe root disk partition has no space for $1M swap file.Script will exit.\n\033[0m"
+		echo -e "\033[1;40;31mThe root disk partition has no space for $1M swap file. Script will exit.\n\033[0m"
 		rm -rf $LOCKfile
 		exit 1
 	fi
 	if [ -e $swapfile ]
 	then
-		echo -e "\033[1;40;31mThe /var/swap_file already exists.Will remove.\n\033[0m"
+		echo -e "\033[1;40;31mThe /var/swap_file already exists. Removing.\n\033[0m"
 		remove_old_swap
 	fi
 	if [ ! -e $swapfile ]
@@ -108,7 +108,7 @@ create_swap(){
 		/sbin/mkswap $swapfile
 		/sbin/swapon $swapfile
 		/sbin/swapon -s
-		echo -e "\033[40;32mStep 3.Add swap partition successful.\n\033[40;37m"
+		echo -e "\033[40;32mStep 3. Successfully added swap partition.\n\033[40;37m"
 	# else
 	# 	echo -e "\033[1;40;31mThe /var/swap_file already exists.Will exit.\n\033[0m"
 	# 	rm -rf $LOCKfile
@@ -118,7 +118,7 @@ create_swap(){
 
 remove_old_swap()
 {
-	old_swap_file=`grep swap $fstab|grep -v "#"|awk '{print $1}'`
+	old_swap_file=$(grep swap $fstab|grep -v "#"|awk '{print $1}')
 	swapoff $old_swap_file
 	cp -f $fstab ${fstab}_bak
 	sed -i '/swap/d' $fstab
@@ -161,10 +161,10 @@ lockfile(){
 LOCKfile=/tmp/.`basename $0`
 if [ -f "$LOCKfile" ]
 then
-	echo -e "\033[1;40;31mThe script is already exist,please next time to run this script.\n\033[0m"
+	echo -e "\033[1;40;31mThe script already exist, please exit before you run this script again.\n\033[0m"
 	exit
 else
-	echo -e "\033[40;32mStep 1.No lock file,begin to create lock file and continue.\n\033[40;37m"
+	echo -e "\033[40;32mStep 1. No lock file, creating lock file...\n\033[40;37m"
 	touch $LOCKfile
 fi
 }
@@ -173,10 +173,10 @@ run(){
 swapfile=/var/swap_file
 fstab=/etc/fstab
 
-echo -e "\033[40;32mStep 3.Check the memory and swap.\n\033[40;37m"
+echo -e "\033[40;32mStep 3. Checking memory and swap.\n\033[40;37m"
 check_memory_and_swap
 
-echo -e "\033[40;32mStep 4.Begin to modify $fstab.\n\033[40;37m"
+echo -e "\033[40;32mStep 4. Begin to modify $fstab.\n\033[40;37m"
 case "$os_release" in
 redhat|centos)
 	config_rhel_fstab
